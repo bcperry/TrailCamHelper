ac4a20f4cc504597ce4a0281a2844d893a87dd13
bcperry <blainecperry@gmail.com>
2023-10-22 17:53:39 -0500

    Added video ingest and download

--- frontend.py
+++ frontend.py
@@ -2,29 +2,53 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import requests
+from PIL import Image
+import io
+import zipfile
+
+import cv2
+
+import torch
+from transformers import DetrImageProcessor, DetrForObjectDetection
+
+from torchvision.utils import draw_bounding_boxes
+from torchvision.transforms.functional import pil_to_tensor, to_pil_image
+
+# start with important models and helpers
+@st.cache_resource
+def load_model():
+    st.header("Deer Detection Model")
+    processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50")
+    model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50")
+    st.success("Loaded model!")
+
+    model.eval()
+
+    return processor, model
+
+large_animal_list = [19, 20, 21, 23, 24, 25]
+
 
 # https://docs.streamlit.io/library/api-reference
 
-st.title('Deer Locator')
-
-# @st.cache_data
-# def load_data(nrows):
-#     data = pd.read_csv(DATA_URL, nrows=nrows)
-#     lowercase = lambda x: str(x).lower()
-#     data.rename(lowercase, axis='columns', inplace=True)
-#     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-#     return data
-
-def multi_inference(files):
-    url = "http://localhost:8000/uploadfiles/"
-    multi_files = (('files', x) for x in files)  # need to convert to tuples for the requests library
-
-    response = requests.post(url=url, files=multi_files)
-    return response
+st.title('TrailCam Helper')
 
 
+# def multi_inference(files):
+#     url = "http://localhost:8000/uploadfiles/"
+#     multi_files = (('files', x) for x in files)  # need to convert to tuples for the requests library
 
-data = st.file_uploader("upload a video", type=['png', 'jpg'], accept_multiple_files=True)
+#     response = requests.post(url=url, files=multi_files)
+#     return response
+
+processor, model = load_model()
+
+confidence = st.slider('What level of confidence to find deer', 0, 100, 80, help="Lower confidence will find more animals, but may have false positives.")
+
+data = st.file_uploader("Upload Trail Camera Images", 
+                        type=['png', 'jpg', 'webp', 'mp4'], 
+                        accept_multiple_files=True,
+                        help="Select the files you want to check for deer")
 
 
 if len(data) > 0:
@@ -32,23 +56,93 @@ if len(data) > 0:
     # Create a text element and let the reader know the data is loading.
     data_load_state = st.text('Loading data...')
     
-    st.write(data)
+    # res = multi_inference(data)
+
+    # # for debugging 
+    # st.subheader("Request POST Header - just for debugging")
+    # st.json(dict(res.request.headers))
+    # st.subheader("Response Status Code - just for debugging")
+    # st.info(f'Status Code: {res.status_code}')
+    # st.subheader("Response Header - just for debugging")
+    # st.json(dict(res.headers))
+    # st.subheader("Response Content - just for debugging")
+    # st.write(res.content)
+
+    data_load_state.text("Data Loaded")
+
+    detection_list = []
+
+    for file in data:
+
+        # check if the file is a video
+        if 'video' in file.type:
+            st.write('video loading...')
+
+            with open(file.name, mode='wb') as f:
+                f.write(file.read(), ) # save video to disk
+
+            vidcap = cv2.VideoCapture(file.name) # load video from disk
+            cur_frame = 0
+
+            success, frame = vidcap.read() # get next frame from video
+            image = Image.fromarray(frame[:,:,[2,1,0]]) # convert opencv frame (with type()==numpy) into PIL Image
+
+        else:
+
+            image = Image.open(file)
+
+
+
+        inputs = processor(images=image, return_tensors="pt")
+        outputs = model(**inputs)
+
+
+
+        # convert outputs (bounding boxes and class logits) to COCO API
+        # let's only keep detections with score > 0.9
+        target_sizes = torch.tensor([image.size[::-1]])
+        results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=(confidence/100))[0]
+        # st.write(results)
+
+        # check if the image contains any interesting detections
+        if bool(set(large_animal_list).intersection(results['labels'].tolist())):
+            st.write(f"Animal found in {file.name}")
+
+            # if an animal is found, add it to the list for later download
+            detection_list.append(file)
+
+
+            # filter the results to just the animals of interest
+            animal_of_interest = np.where(np.isin(results['labels'], large_animal_list))[0]
+
+
+            im = to_pil_image(
+                draw_bounding_boxes(
+                    pil_to_tensor(image),
+                    results['boxes'][animal_of_interest],
+                    colors="red",
+                    # labels = [f"{model.config.id2label[x]}: conf. {round(results['scores'][animal_of_interest].item(), 3)}" for x in results['labels'][animal_of_interest].tolist()]
+                    labels = [f"Confidence: {round(x*100)}" for x in results['scores'][animal_of_interest].tolist()]
+                            )
+                        )
+            
+            st.image(im)
     
-    res = multi_inference(data)
 
-    # for debugging 
-    st.subheader("Request POST Header - just for debugging")
-    st.json(dict(res.request.headers))
-    st.subheader("Response Status Code - just for debugging")
-    st.info(f'Status Code: {res.status_code}')
-    st.subheader("Response Header - just for debugging")
-    st.json(dict(res.headers))
-    st.subheader("Response Content - just for debugging")
-    st.write(res.content)
+    # write the iamges to a zip folder for download
 
-    data_load_state.text("Done! (using st.cache_data)")
+    zip_buffer = io.BytesIO()
 
-    
+    with zipfile.ZipFile(zip_buffer, "a", zipfile.ZIP_DEFLATED, False) as zip_file:
+        for file in detection_list:
+            zip_file.writestr(file.name, file.getvalue())
+
+    st.download_button("Download Images/Videos with Deer",
+                       file_name="deerImages.zip",
+                       mime="application/zip",
+                       data=zip_buffer
+                       )
+
     
     
 # hour_to_filter = st.slider('hour', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
